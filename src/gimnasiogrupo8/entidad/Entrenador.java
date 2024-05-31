182c35eec6a1c0c86b73983f79b82c3eec63f607
BergoglioValentin <jesusvalentin1997123@gmail.com>
2024-05-31 14:05:31 -0300

    Agregar clase de entidad socio y entrenador

--- src/gimnasiogrupo8/entidad/Entrenador.java
+++ src/gimnasiogrupo8/entidad/Entrenador.java
@@ -1,7 +1,82 @@
-
 package gimnasiogrupo8.entidad;
-
-
 public class Entrenador {
-    
+    private int id_Entrenador;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String especialidad;
+    private String disponibilidad;
+
+    public Entrenador() {
+    }
+
+    public Entrenador(int id_Entrenador, String dni, String nombre, String apellido, String especialidad, String disponibilidad) {
+        this.id_Entrenador = id_Entrenador;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+        this.disponibilidad = disponibilidad;
+    }
+
+    public Entrenador(String dni, String nombre, String apellido, String especialidad, String disponibilidad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+        this.disponibilidad = disponibilidad;
+    }
+
+    public int getId_Entrenador() {
+        return id_Entrenador;
+    }
+
+    public void setId_Entrenador(int id_Entrenador) {
+        this.id_Entrenador = id_Entrenador;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(String disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrenador{" + "id_Entrenador=" + id_Entrenador + ", dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", especialidad=" + especialidad + ", disponibilidad=" + disponibilidad + '}';
+    }
 }
--- src/gimnasiogrupo8/entidad/Socio.java
+++ src/gimnasiogrupo8/entidad/Socio.java
@@ -1,7 +1,105 @@
-
 package gimnasiogrupo8.entidad;
-
-
 public class Socio {
+    private int id_socio;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String correo;
+    private String telefono;
+
+    public Socio() {
+    }
+
+    public Socio(int id_socio, String dni, String nombre, String apellido, int edad, String correo, String telefono) {
+        this.id_socio = id_socio;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Socio(String dni, String nombre, String apellido, int edad, String correo, String telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public int getId_socio() {
+        return id_socio;
+    }
+
+    public void setId_socio(int id_socio) {
+        this.id_socio = id_socio;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Socio{" + "id_socio=" + id_socio + ", dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", correo=" + correo + ", telefono=" + telefono + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
